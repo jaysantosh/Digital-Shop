8406c59e075333349ee545aadf58c3d22c8751b6
jaysantosh <161743317+jaysantosh@users.noreply.github.com>
2025-03-23 15:50:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(
       resave: false,
       saveUninitialized: false,
       store: MongoStore.create({ mongoUrl: `${process.env.MONGO_URL}` }),
-      cookie: { secure: false, httpOnly: true , maxAge : 1000 * 60 * 60 * 24 },
+      cookie: { secure: true, httpOnly: true , maxAge : 1000 * 60 * 60 * 24, sameSite:"none" },
     })
   )
 
